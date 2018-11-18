7f0b9a2a9aa64742d5afd65ea4a6bfa1138930d0
pureyoongilove <44716132+pureyoongilove@users.noreply.github.com>
2018-11-18 14:25:45 -0200

    Add files via upload

---
+++ visualizar_pag.php
@@ -0,0 +1,176 @@
+<?php
+//session_start();
+include ("conexao.php");
+if(isset($_SESSION['prof'])|| isset($_SESSION['adm'])){
+	//echo"bem vindo $_SESSION[adm]";
+}else{header("location:login.php");}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	 <title> SCA </title>
+	 <meta charset="utf-8">
+	 <link rel="icon" type="imagem/png" href="img/logo.png" />
+	 
+	 <meta name="viewport" content="width=device-width, initial-scale=1">
+	 <!------- estilo NAVBAR --------->
+	 <link rel="stylesheet" type="text/css" href="css/style.css"/>
+	 <!------- estilo LISTA CLIENTES --------->	
+     <link rel="stylesheet" href="css/bootstrap.css">
+     <link rel="stylesheet" href="css/estilo.css">
+<!------- mascara FORMULARIO ------->
+<script>
+function formatar(mascara, documento){
+  var i = documento.value.length;
+  var saida = mascara.substring(0,1);
+  var texto = mascara.substring(i)
+  
+  if (texto.substring(0,1) != saida){
+            documento.value += texto.substring(0,1);
+  }
+  
+}
+</script>
+
+<style type="text/css">
+body{
+	 background: url(img/cadastro.jpg) no-repeat 0px 0px;
+	 background-attachment: fixed;
+ }
+ 
+label, h2 {
+	color:white;
+} 
+</style>
+</head>
+
+<body>
+
+	<!---------------------------- NAVBAR ---------------------------->
+		<div class="topnav" id="myTopnav">
+				<a>
+				  <img src="img/logoNav.png" alt="logo" style="width:20px">
+				</a>
+			<?php
+			if(isset($_SESSION['adm'])){
+				echo"<a href='indexAdm.php' class='active'>Home</a>			
+			<a href='buscar.php'>Gerenciar alunos</a>
+			<a href='busca_prof.php'>Gerenciar professores</a>			
+			<a href='inadimplentes.php'>Alunos inadimplentes</a>
+			<a href='pagamento.php'>Pagamentos</a>
+			<a href='cadAluno.php'>Cadastrar aluno</a>
+			<a href='cadProf.php'>Cadastrar professor</a>
+			<a href='sair.php'><img src='img/sair_icon.png' alt='Academia' width='25'> Sair</a>
+			<a href='javascript:void(0);' class='icon' onclick='myFunction()'>
+			<img src='img/bars_icon.png' alt='Academia' width='25'>			
+			</a>";
+			}
+			if(isset($_SESSION['prof'])){
+				echo"<a href='indexProf.php' class='active'>Home</a>			
+			<a href='buscar.php'>Gerenciar alunos</a>									
+			<a href='inadimplentes.php'>Alunos inadimplentes</a>
+			<a href='pagamento.php'>Pagamentos</a>
+			<a href='cadAluno.php'>Cadastrar aluno</a>
+			<a href='javascript:void(0);' class='icon' onclick='myFunction()'>
+			<img src='img/bars_icon.png' alt='Academia' width='25'>
+			<div class='topnav' id='iconNav'>			
+				<a href='sair.php'><img src='img/sair_icon.png' alt='Academia' width='25'> Sair</a>
+			</div>
+			</a>";
+				
+			}				
+			?>
+	    </div>
+		
+		<!---------------------------- SCRIPT NAVBAR ---------------------------->
+	<script>
+		function myFunction() {
+		var x = document.getElementById("myTopnav");
+		if (x.className === "topnav") {
+        x.className += " responsive";
+			} else {
+        x.className = "topnav";
+		   }
+		}
+	</script>	
+
+	<br/><br/>
+
+	<h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Visualizar pagamentos <img src="img/pag_icon.png" alt="Academia" width="30"></h2>
+	<br/><br/><br/><br/>
+	
+<form method="POST">
+<div align="center">
+  <div class="form-group col-md-4"> 
+	<label for="cpf">CPF:</label>
+	<input type="text" class="form-control" name="busca" maxlength="14" OnKeyPress="formatar('###.###.###-##', this)" required>
+	<br/><br/>
+	<button class="btn btn-success btn-xs" type="submit" name="Buscar"> Verificar pagamentos <img src="img/ok_icon.png" alt="Academia" width="25"></button>
+	<a href="pagamento.php"><button type="button" class="btn btn-danger btn-xs"> Calcelar <img src="img/cancel_icon.png" alt="Academia" width="18"></button>
+  </div>
+</div>
+</form>
+
+	<br/><br/><br/>
+
+		<div style="overflow-x:auto;">
+		<div class="container">         		  
+            <table class="table table table-dark table-hover">
+                <thead>
+                  <tr>                                      				
+                    <th>DATA</th>
+					<th>VALOR</th>	
+                  </tr>
+                </thead>
+                <tbody>
+
+<?php
+//include("conexao.php");
+$con = $_SESSION['con'];
+if(isset($_POST['Buscar'])){
+$cpf = $_POST['busca'];
+$id=0;
+$sql= "SELECT * FROM cliente WHERE cpf = '$cpf'";
+$pesquisa = mysqli_query($con,$sql);
+if (!$pesquisa) {
+                    echo "  <div align='center'>
+						<div class='alert alert-warning'>				
+						<strong>Erro no pagamento!</strong> O que deseja fazer agora?
+						</div>
+						</div>.";
+                    
+                }
+while ($dados = mysqli_fetch_assoc($pesquisa)) {
+                    
+                   $id = $dados['id'];
+}
+$sql = "SELECT * FROM mensalidade WHERE id_cliente = '$id'";
+$mensa = mysqli_query($con,$sql);
+
+while ($dados = mysqli_fetch_assoc($mensa)){
+    
+                echo "<tr>";               
+                //echo "<td>" .$dados['id_mensalidade']. "</td>";
+                echo "<td>" .$dados['data']. "</td>";
+                echo "<td>" .$dados['valor']. "</td>";
+				echo "<tr>"; 
+
+	
+}
+}
+
+?>
+
+    </tbody>
+              </table>            
+        </div>
+	</div>
+		 
+		<script src="jquery/dist/jquery.js"></script>
+        <script src="popper.js/dist/popper.min.js"></script>
+        <script src="js/bootstrap.js"></script>
+
+    </body>
+</html>
--- visualizar_pagamento.php
+++ visualizar_pagamento.php
@@ -6,11 +6,11 @@ if(isset($_SESSION['prof'])|| isset($_SESSION['adm'])){
 }else{header("location:login.php");}
 ?>
 
+<!DOCTYPE html><
+html lang="pt-br">
 
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
+<html>
+    <head>
 	 <title> SCA </title>
 	 <meta charset="utf-8">
 	 <link rel="icon" type="imagem/png" href="img/logo.png" />
@@ -43,13 +43,16 @@ body{
  
 label, h2 {
 	color:white;
-} 
-</style>
-	 
-</head>
+}
 
-    <body>
-	
+div.alert {
+    width: 70%;
+}
+</style>
+    </head>
+   
+   <body>
+
 	<!---------------------------- NAVBAR ---------------------------->
 		<div class="topnav" id="myTopnav">
 				<a>
@@ -98,73 +101,55 @@ label, h2 {
 		}
 	</script>	
 
-	<br/><br/>
-	
-        <script>
-            function mascara(o,f){
-v_obj=o;
-v_fun=f;
-setTimeout("execmascara()",1);
-}
-function execmascara(){
-v_obj.value=v_fun(v_obj.value);
-}
-            function moeda(z){ 
-v = z.value; 
-v=v.replace(/\D/g,""); // permite digitar apenas numero 
-v=v.replace(/(\d{1})(\d{17})$/,"$1.$2"); // coloca ponto antes dos ultimos digitos 
-v=v.replace(/(\d{1})(\d{13})$/,"$1.$2"); // coloca ponto antes dos ultimos 13 digitos 
-v=v.replace(/(\d{1})(\d{10})$/,"$1.$2"); // coloca ponto antes dos ultimos 10 digitos 
-v=v.replace(/(\d{1})(\d{7})$/,"$1.$2"); // coloca ponto antes dos ultimos 7 digitos 
-v=v.replace(/(\d{1})(\d{1,4})$/,"$1,$2") ;// coloca virgula antes dos ultimos 4 digitos 
-z.value = v; 
-}
-        </script>
-		
-	<h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Adicionar pagamentos <img src="img/pag_icon.png" alt="Academia" width="30"></h2>
-	<br/><br/><br/><br/>
-		
-        <form action="pagar.php" method="POST">
-		
-  <div align="center">
-  
-    <div class="form-group col-md-4">
-      <label for="cpf">CPF:</label>
-      <input type="text" class="form-control" name="cpf" value="" maxlength="14" OnKeyPress="formatar('###.###.###-##', this)" required>
-    </div>
-	
-    <div class="form-group col-md-4">
-    <div class="form-group">
-      	<label for="valor">Valor:</label>
-      	<select name="valor" class="form-control" required>
-          <option name="valor" value="10.00">R$10,00</option>
-          <option name="valor" value="35.00">R$35,00</option>
-		  <option name="valor" value="45.00">R$45,00</option>
-		  <option name="valor" value="65.90">R$65,90</option>
-          <option name="valor" value="75.90">R$75,90</option>
-          <option name="valor" value="85.90">R$85,90</option>
-		  <option name="valor" value="100.00">R$100,00</option>       					
-      	</select>     
-   </div>				
-   </div>
-   
-  </div>
+	<br/><br/>   
+        
+<?php
+        include("conexao.php");
+        $con = $_SESSION['con'];
  
-  <div align="center">
-  <div id="actions" class="row">
-    <div class="col-md-12">
-      <br/><br/><br/>
-	  <button class="btn btn-success btn-xs" type="submit" name="enviar"> Efetuar pagamento <img src="img/ok_icon.png" alt="Academia" width="25"></button>
-      <a href="visualizar_pag.php"><button type="button" class="btn btn-secondary btn-xs"> Verificar pagamentos <img src="img/pagInfo_icon.png" alt="Academia" width="18"></button></a>
-	  <a href="buscar.php"><button type="button" class="btn btn-danger btn-xs"> Calcelar <img src="img/cancel_icon.png" alt="Academia" width="18"></button></a>
-    </div>
-  </div>
-  </div>
-	
-        </form>
-        <?php
-        // put your code here
-        ?>
-		
+  $sql = "SELECT MAX(id_mensalidade) FROM mensalidade where id_cliente = 1" ;
+  
+  $res = mysqli_query($con, $sql);
+ 
+  while ($dados = mysqli_fetch_assoc($res)) {
+                    
+                   $id = $dados['MAX(id_mensalidade)'];
+}
+  echo"$id";
+   
+  $sql1 = "SELECT * FROM `mensalidade` WHERE `id_mensalidade` = $id";
+  $mensa = mysqli_query($con,$sql1);
+  if (!$mensa) {
+                    echo "<div class='alert alert-danger alert-dismissible fade show'>
+					<button type='button' class='close' data-dismiss='alert'>&times;</button>
+					<strong>Erro ao realizar consulta!</strong> Tente novamente.
+					</div>";
+                    
+                }
+ else{ while ($da = mysqli_fetch_assoc($mensa)){
+                    
+                   echo $da['id_mensalidade']."<br/>";
+                 $data = $da['data'];
+                   echo $da['valor']."<br/><br/>";
+    
+ }
+ echo "$data"."<br/>";
+ //$data = explode("-",$data);
+ //echo $data[0];
+ 
+ $data1 = date("Y/m/d");
+     str_replace('/','-',$data);
+     
+     $d1 = strtotime($data1); 
+$d2 = strtotime($data);
+
+$dataFinal = ($d2 - $d1) /86400;
+if($dataFinal < 0){
+$dataFinal = $dataFinal * -1;}
+
+echo "Entre as duas datas informadas, existem $dataFinal dias.";
+}
+?>
+
     </body>
 </html>
